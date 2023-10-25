1a71904bd45844422b4e421fd1181a7b61da8d6a
PRich57 <pcrichards57@gmail.com>
2023-10-25 10:05:13 -0600

    Starter component styling

--- src/sections/Starter.jsx
+++ src/sections/Starter.jsx
@@ -14,36 +14,40 @@ function Starter() {
     <div className="main-container">
       <div className="container">
         <div className="content">
-          <h1 
-            style= {{
-              fontSize: fontSize,
-              color: textColor
-            }}
-          >
-            <TypeAnimation 
-              sequence={[
-                `Pete Richards`,
-                1000, 
-                () => setTextColor('#f32a8f'),
-                () => setFontSize('2rem'),
-                `React Practice`
-              ]}
-              speed={30}
-              cursor={false}
-              omitDeletionAnimation={true}
-            />
+          <h1>Pete Richards
           </h1>
+            <div 
+              style= {{
+                fontSize: fontSize,
+                color: textColor,
+                paddingBottom: '2rem',
+                paddingTop: '0'
+              }}
+            >
+              <TypeAnimation 
+                sequence={[
+                  ``,
+                  1000, 
+                  () => setTextColor('#ffffff'),
+                  () => setFontSize('2rem'),
+                  `\nWelcome to my Portfolio`
+                ]}
+                speed={30}
+                cursor={false}
+                omitDeletionAnimation={true}
+              />
+            </div>
           <div className="button-container">
-            <Button content="Watch Video" icon={<GoPlay />} />
+            <Button content="Contact" icon={<GoPlay />} />
             <Button
-              content="Request Quote"
+              content="Menu"
               icon={<HiOutlineArrowNarrowRight />}
               color="pink"
             />
           </div>
         </div>
         <div className="image">
-          <img src="../src/assets/react.svg" alt="React Icon" />
+          <img src="../src/assets/linkedin_profile_pic.jpg" alt="React Icon" />
         </div>
       </div>
     </div>
@@ -53,7 +57,7 @@ function Starter() {
 export default Starter;
 
 
-<TypeAnimation 
+{/* <TypeAnimation 
 sequence={[
   `Pete Richards`,
   2000,
@@ -62,4 +66,4 @@ speed={30}
 cursor={false}
 style={{ whiteSpace: 'pre-line', fontSize: '2em' }}
 omitDeletionAnimation={true}
-/>
+/> */}
