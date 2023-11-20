37f0d9cad939e54cdbd1eaf6ebe5dcc2933e5e7d
PRich57 <pcrichards57@gmail.com>
2023-11-19 17:50:04 -0700

    Begin refactoring resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -7,8 +7,157 @@ import Button from "../components/Button"
 export default function Resume() {
   return (
     <>
+      <div className="acrossBottom">
+        <h4 className="resumeSections">
+          Technologies
+        </h4>
+        <img
+          src="https://simpleicons.now.sh/javascript/F7DF1E"
+          alt="JavaScript"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/react/FF4154"
+          alt="React"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/mongodb/47A248"
+          alt="MongoDB"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/bootstrap/7952b3"
+          alt="Bootstrap"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/express/ffffff"
+          alt="express"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/nodedotjs/339933"
+          alt="Node.js"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/graphql/e10098"
+          alt="GraphQL"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/apollographql/2196f0"
+          alt="Apollo"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/mysql/4479a1"
+          alt="mysql"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/npm/cb3837"
+          alt="npm"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/vite/646cff"
+          alt="vite"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/heroku/430098"
+          alt="Heroku"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/netlify/00C7B7"
+          alt="Netlify"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/insomnia/4000BF"
+          alt="Insomnia"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+        <img
+          src="https://simpleicons.now.sh/mui/007fff"
+          alt="MUI"
+          width="40"
+          height="40"
+          className="tech-icons"
+        />
+      </div>
       <div className="resume">
-          <img src={resume} alt="Pete's Resume" />
+        <div className="left">
+          <h4 className="resumeSections">
+            Relevant Experience
+          </h4>
+          <div className="resumeContent">
+            <ul className="resumeList">
+              <li className="resumeItem">content</li>
+            </ul>
+          </div>
+        </div>
+        <div className="right">
+          <h4 className="resumeSections">
+            Skills
+          </h4>
+          <div className="resumeContent">
+          <ul className="resumeList">
+              <li className="resumeItem">content</li>
+            </ul>
+          </div>
+        </div>
+        <div className="left">
+          <h4 className="resumeSections">
+            Education
+          </h4>
+          <div className="resumeContent">
+          <ul className="resumeList">
+              <li className="resumeItem">content</li>
+            </ul>
+          </div>
+        </div>
+        <div className="right">
+          <h4 className="resumeSections">
+            Achievements
+          </h4>
+          <div className="resumeContent">
+          <ul className="resumeList">
+              <li className="resumeItem">content</li>
+            </ul>
+          </div>
+        </div>
       </div>
       <div className="button">
         <a href={pdf} target='_blank'> 
