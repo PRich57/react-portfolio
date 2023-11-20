b3463802aa98467ea8c4f2af2404788702da0b5f
PRich57 <pcrichards57@gmail.com>
2023-11-20 09:34:24 -0700

    Start building out new resume data

---
+++ src/utils/resumeSkills.js
@@ -0,0 +1,33 @@
+export const resumeSkills = [
+  {
+    title: `Technical Skills`,
+    languages: [
+      {
+        type: "Development",
+        name: ["JavaScript - Proficient", "HTML/CSS - Proficient", ],
+      },
+      {
+        type: "Database",
+        name: ["GraphQL/Apollo - Proficient", "SQL - Intermediate"],
+      },
+      {
+        type: "Programming",
+        name: ["C - Intermediate", "Python - Intermediate"],
+      },
+    ],
+    tools: [
+      {
+        type: "IDE",
+        name: ["VS Code - Proficient"]
+      },
+      {
+        type: "Database & Database Libraries",
+        name: ["MongoDB/Mongoose - Proficient", "MySQL/Sequelize - Intermediate"]
+      },
+      {
+        type: "IDE",
+        name: ["VS Code"]
+      },
+    ],
+  }
+]
