cb42f4736e99e862812b1b879634ed4f98dbfaab
PRich57 <pcrichards57@gmail.com>
2023-11-24 12:05:03 -0700

    Create a timer displaying data-info when not hovering over icons

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import pdf from "/src/assets/resume.pdf";
 import "../styles/components/Resume.css";
 import { Download } from "react-bootstrap-icons";
@@ -10,19 +10,51 @@ import Skills from "../components/Resume/Skills"
 export default function Resume() {
   const [hiddenWord, setHiddenWord] = useState("");
   const [tooltipPosition, setTooltipPosition] = useState({ top: 0, left: 0 });
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isHovering, setIsHovering] = useState(false);
+
+
+  useEffect(() => {
+    let interval;
+    if (!isHovering) { 
+      interval = setInterval(() => {
+        setCurrentIndex(prevIndex =>
+          prevIndex === resumeIcons.length - 1 ? 0 : prevIndex + 1
+        );
+      }, 2000);
+    }
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isHovering]); 
 
   const handleMouseEnter = (e) => {
+    setIsHovering(true);
     setHiddenWord(e.target.getAttribute("data-info"));
     const rect = e.target.getBoundingClientRect();
     setTooltipPosition({
-      top: rect.bottom + window.scrollY, // position below the image
-      left: rect.left + window.scrollX + (rect.width / 2) // horizontally centered
+      top: rect.bottom + window.scrollY, 
+      left: rect.left + window.scrollX + (rect.width / 2)
     });
   }
 
   const handleMouseLeave = () => {
+    setIsHovering(false);
     setHiddenWord('');
   }
+
+  useEffect(() => {
+    const icon = resumeIcons[currentIndex];
+    setHiddenWord(icon.data);
+    const element = document.querySelector(`[data-info="${icon.data}"]`);
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      setTooltipPosition({
+        top: rect.bottom + window.scrollY,
+        left: rect.left + window.scrollX + (rect.width / 2)
+      });
+    }
+  }, [currentIndex]);
 
   return (
     <>
@@ -61,9 +93,6 @@ export default function Resume() {
           <h4 className="resumeSections">Skills</h4>
           <div className="resumeContent skills">
             <Skills/>
-            {/* <ul className="resumeList">
-              <li className="resumeItem">content</li>
-            </ul> */}
           </div>
         </div>
         <div className="right">
--- src/utils/resumeUtils/resumeIcons.js
+++ src/utils/resumeUtils/resumeIcons.js
@@ -2,76 +2,81 @@ export const resumeIcons = [
   {
     src: "https://simpleicons.now.sh/react/FF4154",
     alt: "React icon",
-    data: "React"
+    data: "React - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/mongodb/47A248",
     alt: "MongoDB icon",
-    data: "MongoDB"
+    data: "MongoDB - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/express/ffffff",
     alt: "Express.js icon",
-    data: "Express.js"
+    data: "Express.js - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/nodedotjs/339933",
     alt: "Node.js icon",
-    data: "Node.js"
+    data: "Node.js - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/graphql/e10098",
     alt: "GraphQL icon",
-    data: "GraphQL"
+    data: "GraphQL - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/apollographql/2196f0",
     alt: "Apollo icon",
-    data: "Apollo"
+    data: "Apollo - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/mysql/4479a1",
     alt: "MySQL icon",
-    data: "MySQL"
+    data: "MySQL - Intermediate"
   },
   {
     src: "https://simpleicons.now.sh/npm/cb3837",
     alt: "npm icon",
-    data: "npm"
+    data: "npm - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/vite/646cff",
     alt: "Vite icon",
-    data: "Vite"
+    data: "Vite - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/heroku/430098",
     alt: "Heroku icon",
-    data: "Heroku"
+    data: "Heroku - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/netlify/00C7B7",
     alt: "Netlify icon",
-    data: "Netlify"
+    data: "Netlify - Proficient"
+  },
+  {
+    src: "https://simpleicons.vercel.app/visualstudiocode/0078d7",
+    alt: "Visual Studio Code icon",
+    data: "VSCode - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/insomnia/4000BF",
     alt: "Insomnia icon",
-    data: "Insomnia"
+    data: "Insomnia - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/mui/007fff",
     alt: "Material UI icon",
-    data: "Material UI"
+    data: "Material UI - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/bootstrap/7952b3",
     alt: "Bootstrap icon",
-    data: "Bootstrap"
+    data: "Bootstrap - Proficient"
   },
   {
     src: "https://simpleicons.now.sh/git/f34f29",
     alt: "Git icon",
-    data: "Git"
+    data: "Git - Proficient"
   },
 ]
