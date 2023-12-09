d5f2abc58e64ef6e3a6a480abc3a64b90475cde5
PRich57 <pcrichards57@gmail.com>
2023-12-09 13:58:45 -0700

    Styling consistencey fix

--- src/utils/resumeUtils/resumeEducation.js
+++ src/utils/resumeUtils/resumeEducation.js
@@ -4,22 +4,22 @@ export const resumeEducation = [
     school: 'Ohio University',
     degree: 'Bachelor of Science in Integrated Media',
     minor: 'Business Administration',
-    startYear: 2011,
-    endYear: 2016,
+    startYear: "Aug 2011",
+    endYear: "May 2016",
     location: 'Athens, OH',
   },
   {
     school: 'University of Denver',
     degree: 'Full Stack Web Development Certificate',
-    startYear: "Aug. 2023",
-    endYear: "Nov. 2023",
+    startYear: "Aug 2023",
+    endYear: "Nov 2023",
     location: 'Denver, CO'
   },
   {
     school: 'Harvard University',
     degree: 'CS50 Certificate of Completion',
     startYear: 'May 2023',
-    endYear: 'Dec. 2023',
+    endYear: 'Dec 2023',
     location: 'Remote (edX)'
   },
 ]
