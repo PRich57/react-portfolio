ab86defc94c0c132a2fa36075a95dea8d934776e
PRich57 <pcrichards57@gmail.com>
2024-07-22 11:15:40 -0600

    Remove depricated icon from resume page

--- src/utils/resumeUtils/resumeIcons.js
+++ src/utils/resumeUtils/resumeIcons.js
@@ -2,22 +2,22 @@ export const resumeIcons = [
   {
     src: "https://simpleicons.vercel.app/javascript/F0DB4F",
     alt: "JavaScript icon",
-    data: "JavaScript - Proficient"
+    data: "JavaScript - Advanced"
   },
   {
     src: "https://simpleicons.vercel.app/c/044F88",
     alt: "C icon",
-    data: "C - Intermediate"
+    data: "C - Proficient"
   },
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/768px-Python.svg.png",
     alt: "Python icon",
-    data: "Python - Intermediate"
+    data: "Python - Advanced"
   },
   {
     src: "https://simpleicons.now.sh/react/FF4154",
     alt: "React icon",
-    data: "React - Proficient"
+    data: "React - Advanced"
   },
   {
     src: "https://simpleicons.vercel.app/jquery/0769AD",
@@ -37,12 +37,12 @@ export const resumeIcons = [
   {
     src: "https://simpleicons.now.sh/express/ffffff",
     alt: "Express.js icon",
-    data: "Express.js - Proficient"
+    data: "Express.js - Advanced"
   },
   {
     src: "https://simpleicons.now.sh/nodedotjs/339933",
     alt: "Node.js icon",
-    data: "Node.js - Proficient"
+    data: "Node.js - Advanced"
   },
   {
     src: "https://simpleicons.now.sh/graphql/e10098",
@@ -57,12 +57,12 @@ export const resumeIcons = [
   {
     src: "https://simpleicons.now.sh/mysql/4479a1",
     alt: "MySQL icon",
-    data: "MySQL - Intermediate"
+    data: "MySQL - Advanced"
   },
   {
     src: "https://simpleicons.now.sh/npm/cb3837",
     alt: "npm icon",
-    data: "npm - Proficient"
+    data: "npm - Advanced"
   },
   {
     src: "https://simpleicons.now.sh/vite/646cff",
@@ -80,28 +80,23 @@ export const resumeIcons = [
     data: "Netlify - Proficient"
   },
   {
-    src: "https://simpleicons.vercel.app/visualstudiocode/0078d7",
-    alt: "Visual Studio Code icon",
-    data: "VSCode - Proficient"
-  },
-  {
     src: "https://simpleicons.now.sh/insomnia/4000BF",
     alt: "Insomnia icon",
-    data: "Insomnia - Proficient"
+    data: "Insomnia - Advanced"
   },
   {
     src: "https://simpleicons.now.sh/mui/007fff",
     alt: "Material UI icon",
-    data: "Material UI - Proficient"
+    data: "Material UI - Advanced"
   },
   {
     src: "https://simpleicons.now.sh/bootstrap/7952b3",
     alt: "Bootstrap icon",
-    data: "Bootstrap - Proficient"
+    data: "Bootstrap - Advanced"
   },
   {
     src: "https://simpleicons.now.sh/git/f34f29",
     alt: "Git icon",
-    data: "Git - Proficient"
+    data: "Git - Advanced"
   },
 ]
